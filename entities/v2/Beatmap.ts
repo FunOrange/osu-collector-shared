f090e44edfd77a7cf61ad25f413c6d2d69045711
Jun Villa <jun@aucto.com>
2025-02-20 19:26:02 -0500

    move entities into v2 folder

---
+++ entities/v2/Beatmap.ts
@@ -0,0 +1,27 @@
+import { Beatmapset } from "./Beatmapset";
+import { pick } from "ramda";
+
+export const BeatmapSample = {
+  beatmapset_id: 3,
+  id: 53,
+  checksum: "1d23c37a2fda439be752ae2bca06c0cd",
+  version: "-Crusin-",
+  mode: "osu" as "osu" | "taiko" | "mania" | "fruits",
+  difficulty_rating: 1.83,
+  accuracy: 4,
+  drain: 3,
+  bpm: 172,
+  cs: 5,
+  ar: 4,
+  hit_length: 77,
+  status: "ranked",
+};
+export type Beatmap = typeof BeatmapSample;
+export const BeatmapKeys = Object.keys(BeatmapSample) as (keyof Beatmap)[];
+export interface BeatmapWithBeatmapset extends Beatmap {
+  beatmapset: Beatmapset;
+}
+
+export function trimRawBeatmap(doc: any): Beatmap {
+  return pick(BeatmapKeys, doc);
+}
