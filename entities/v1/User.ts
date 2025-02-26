f2a93cdf628cc73864ae5ac760ab8331241fed02
Jun Villa <jun@aucto.com>
2025-02-26 12:05:43 -0500

    add converter

--- entities/v1/User.ts
+++ entities/v1/User.ts
@@ -1,3 +1,4 @@
+import { UserPrivate } from '../v2/UserPrivate';
 import { FirestoreTimestamp } from './FirestoreTimestamp';
 
 export interface User {
@@ -37,6 +38,22 @@ interface Private {
   subscriptionExpiryDate?: FirestoreTimestamp;
   paypalSubscriptionId?: string;
 }
+
+export const toV2UserPrivate = (userId: number, privateData: Private): UserPrivate => {
+  if (!privateData) return null;
+  return {
+    user_id: userId,
+    twitch_display_name: privateData.linkedTwitchAccount?.displayName ?? null,
+    twitch_name: privateData.linkedTwitchAccount?.name ?? null,
+    twitch_id: privateData.linkedTwitchAccount?.id ?? null,
+    twitch_error: privateData.twitchError || false,
+    twitch_token: privateData.twitchToken ?? null,
+    stripe_subscription_id: privateData.stripeSubscriptionId ?? null,
+    stripe_customer_id: privateData.stripeCustomer?.id ?? null,
+    stripe_customer_email: privateData.stripeCustomer?.email ?? null,
+    paypal_subscription_id: privateData.paypalSubscriptionId ?? null,
+  };
+};
 
 interface LinkedTwitchAccount {
   displayName: string;
